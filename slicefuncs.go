5b69a0472cb2612cc8d562712de427537a9376f5
cdillond <93200981+cdillond@users.noreply.github.com>
2023-01-19 22:52:11 -0500

    added info

--- slicefuncs.go
+++ slicefuncs.go
@@ -1,10 +1,13 @@
+// Package slicefuncs provides utility functions for slices in Go, loosely based on functions from Python and Java's standard libraries.
+// It is not stable or thoroughly tested.
+
 package slicefuncs
 
 import (
 	"fmt"
 )
 
-// Returns the index of the first occurence of `v` in `s` for built-in types that satisfy the `comparable` interface.
+// Returns the index of the first occurence of v in s for built-in types that satisfy the comparable interface.
 func IndexOf[C comparable](v C, s []C) (int, error) {
 	for i := 0; i < len(s); i++ {
 		if v == s[i] {
@@ -14,8 +17,8 @@ func IndexOf[C comparable](v C, s []C) (int, error) {
 	return -1, fmt.Errorf("not found")
 }
 
-// Returns the index of the first occurence of `v` in `q`. `equals` must return `true` when `a==b` and `false` when `a!=b`.
-// This function is intended to accomodate types that cannot be used with the built-in comparison operators (and therefore do not work with `IndexOf`).
+// Returns the index of the first occurence of v in q. equals must return true when a==b and false when a!=b.
+// This function is intended to accomodate types that cannot be used with the built-in comparison operators (and therefore do not work with IndexOf).
 func IndexOfAny[T any](v T, s []T, equals func(a, b T) bool) (int, error) {
 	for i := 0; i < len(s); i++ {
 		if equals(v, s[i]) {
@@ -25,7 +28,7 @@ func IndexOfAny[T any](v T, s []T, equals func(a, b T) bool) (int, error) {
 	return -1, fmt.Errorf("not found")
 }
 
-// Returns the result of `fn(s[i])` for all integer values of `i` between `0` and `len(s)`.
+// Returns the result of fn(s[i]) for all integer values of i between 0 and len(s).
 func MapTo[T, R any](s []T, fn func(t T) R) []R {
 	res := make([]R, len(s))
 	for i := 0; i < len(s); i++ {
@@ -34,7 +37,7 @@ func MapTo[T, R any](s []T, fn func(t T) R) []R {
 	return res
 }
 
-// Returns a slice of all elements of `s` that satisfy the condtion determined by `fn`.
+// Returns a slice of all elements of s that satisfy the condtion determined by fn.
 func Filter[T any](s []T, fn func(t T) bool) []T {
 	// pre-allocate res based on its max potential size
 	res := make([]T, 0, len(s))
@@ -47,8 +50,8 @@ func Filter[T any](s []T, fn func(t T) bool) []T {
 	return res[:len(res):len(res)]
 }
 
-// Returns a slice of the accumulated results of `fn` applied to `s`.
-// When `len(s) > 0`, `s[0]` will always be equal to the value in the initial position of the returned slice.
+// Returns a slice of the accumulated results of fn applied to s.
+// When len(s) > 0, s[0] will always be equal to the value in the initial position of the returned slice.
 func Accumulate[T any](s []T, fn func(a, b T) T) []T {
 	res := make([]T, len(s))
 	if len(s) == 0 {
@@ -61,7 +64,7 @@ func Accumulate[T any](s []T, fn func(a, b T) T) []T {
 	return res
 }
 
-// Returns a reversed copy of `s`.
+// Returns a reversed copy of s.
 func Reverse[T any](s []T) []T {
 	res := make([]T, len(s))
 	for i := 0; i < len(s); i++ {
@@ -70,7 +73,7 @@ func Reverse[T any](s []T) []T {
 	return res
 }
 
-// Returns the sum of all elements of `s`. Elements of `s` must instantiate the `Number` interface, which permits all built-in numeric types supported by Go. `Sum` overflows silently and returns a zero value when `s` is an empty slice.
+// Returns the sum of all elements of s. Elements of s must instantiate the Number interface, which permits all built-in numeric types supported by Go. Sum overflows silently and returns a zero value when s is an empty slice.
 func Sum[N Number](s []N) N {
 	var res N
 	for i := 0; i < len(s); i++ {
@@ -79,7 +82,7 @@ func Sum[N Number](s []N) N {
 	return res
 }
 
-// Returns the product of all elements of `s`. Elements of `s` must instantiate the `Number` interface, which permits all built-in numeric types supported by Go. `Prod` overflows silently and returns a zero value when `s` is an empty slice.
+// Returns the product of all elements of s. Elements of s must instantiate the Number interface, which permits all built-in numeric types supported by Go. Prod overflows silently and returns a zero value when s is an empty slice.
 func Prod[N Number](s []N) N {
 	var res N
 	if len(s) < 1 {
@@ -92,7 +95,7 @@ func Prod[N Number](s []N) N {
 	return res
 }
 
-// Returns a slice of type `[]T` with a length of `n` where all elements are set to `v`.
+// Returns a slice of type []T with a length of n where all elements are set to v.
 func Repeat[T any](v T, n int) ([]T, error) {
 	var res []T
 	if n < 0 {
@@ -105,6 +108,7 @@ func Repeat[T any](v T, n int) ([]T, error) {
 	return res, nil
 }
 
+// Combines all elements in a slice of slices into a single slice.
 func Flatten[T any](s [][]T) []T {
 	// determining the length of the final slice in advance to avoid unecessary allocations
 	var l int
